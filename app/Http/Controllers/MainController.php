a3f3e81fda5fc39f8a651c3d1c7ef2cf46989d91
Volodymyr Novak <novakvova@gmail.com>
2020-11-26 17:48:06 +0200

    Add migrations and base controller

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class MainController extends Controller
+{
+
+    public function Index(Request $request)
+    {
+//        $categories = Category::query()->get();
+//        dd($categories);
+
+//        $posts = Post::query()->get();
+//        dd($posts);
+
+//        $posts = Post::query()->with('Category')->get();
+//        $category = $posts[0]->Category;
+//        dd($category->name);
+
+        $posts = Post::query()->with('Tags')->get();
+
+        foreach ($posts[0]->Tags as $tag) {
+            echo $tag->name. '<br />';
+        }
+
+        dd($posts);
+        return 1;
+    }
+}
