848bdc3f026936d14c0c7d6495a280c09962a2aa
Volodymyr Novak <novakvova@gmail.com>
2020-12-01 17:53:49 +0200

    import app required

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,11 +5,32 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{mix('/css/app.css')}}" />
     <title>
         @yield('title')
     </title>
 </head>
 <body>
+    @include('layouts._navbar')
+
+    <!-- Page Header -->
+    <header class="masthead" style="background-image: url('/img/home-bg.jpg')">
+        <div class="overlay"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-md-10 mx-auto">
+                    <div class="site-heading">
+                        <h1>Clean Blog</h1>
+                        <span class="subheading">A Blog Theme by Start Bootstrap</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+
     @yield('content')
+
+    <script src="{{mix('/js/app.js')}}"></script>
 </body>
 </html>
